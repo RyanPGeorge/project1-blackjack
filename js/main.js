74f1e688f84265571c8fc0a032e6b20ea8f75cb7
Ryan George <ryangeorge@Ryans-MacBook-Pro.local>
2019-08-07 17:43:20 -0500

    improve deal and hit functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
+
+
+// deal two cards and by pushing them into player hand array *CHECKKKKKK*
+
+// if player wishes, he can hit, at which point another card will be pushed into the player hand arrr, from the deck
+
+// after each hit, evaluate if player is at 21 or bust. If under 21 allow option to hit to remain open. 
+
+// set up onclick event functions 
+
+/* findSum = (playerHand) => {
+  let hand = [...playerHand]
+
+  hand.forEach(card => {
+    let sum =+ card.value
+    return sum
+  })
+  console.log(sum)
+  checkValue(sum)
+}
+
+checkValue = () => {
+  if (sum > 21) {
+    //bust you loose
+  }
+  else if (sum === 21) {
+    //you win
+  }
+  else {
+    // all hit 
+  }
+}
+*/
+
 /*----- constants -----*/
 var suits = ['s', 'c', 'd', 'h'];
 var ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
@@ -8,15 +42,16 @@ var masterDeck;
 
 /*----- app's state (variables) -----*/
 var shuffledDeck;
-var playerHand;
+var playerHand = [];
+// player hand value
 
 /*----- cached element references -----*/
-var shuffledContainer = document.getElementById('shuffled-deck-container');
+//var shuffledContainer = document.getElementById('shuffled-deck-container');
 var handContainer = document.getElementById('hand-container');
 var dealerContainer = document.getElementById('dealer-container');
 
 /*----- event listeners -----*/
-document.querySelector('button').addEventListener('click', dealPlayerHand, dealDealerHand); 
+document.querySelector('button').addEventListener('click', dealPlayerHand); 
 //document.querySelector('hit').addEventListener('click', hitPlayerHand); 
 //document.querySelector('stay').addEventListener('click', renderShuffledDeck);
 
@@ -24,11 +59,23 @@ document.querySelector('button').addEventListener('click', dealPlayerHand, dealD
 function init(){
     masterDeck = buildMasterDeck();
     shuffledDeck = shuffledDeck();
-    //dealPlayerHand = dealPlayerHand();
-    //hitPlayerHand = hitPlayerHand();
-    //buttons
 }
 init()
+
+function buildMasterDeck() {
+  var deck = [];
+  suits.forEach(function(suit) {
+    ranks.forEach(function(rank) {
+      deck.push({
+        // the 'face' property maps to the CSS classes for cards
+        face: `${suit}${rank}`,
+        // the 'value' property is set for blackjack, not war
+        value: Number(rank) || (rank === 'A' ? 11 : 10)
+      });
+    });
+  });
+  return deck;
+}
 
 function shuffledDeck() {
   // create a copy of the masterDeck (leave masterDeck untouched!)
@@ -39,55 +86,45 @@ function shuffledDeck() {
     shuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
   }
   return shuffledDeck;
-  //renderDeckInContainer(shuffledDeck, shuffledContainer);
 }
 
+function renderDeckInContainer(deck, container) {
+  container.innerHTML = '';
+  // Let's build the cards as a string of HTML
+  var cardsHtml = deck.reduce(function(html, card, value) {
+      //console.log('card face', card.face)
+      //console.log('card value is', value)  
+      //console.log("the player's hand is", playerHand)  
+      ///// higher order array method or for loop to iterate over hand, this function will include the DOM elements to display the card, and values
+    return html + `<div class="card ${card.face}"></div>`;
+  }, '');
+  container.innerHTML = cardsHtml;
+}
 
 // Deal me a hand functions ->
 function dealPlayerHand() {
     var tempDeck = shuffledDeck.slice(0,2);
     playerHand = [];
-    playerHand.push(tempDeck);
-    renderDeckInContainer(playerHand, handContainer);
-  } 
+    playerHand = playerHand.concat(tempDeck);
+    renderDeckInContainer(playerHand, handContainer); 
+}
 
-function dealDealerHand() {
-    var tempDeck = shuffledDeck.slice(0,2);
-    playerHand = [];
-    playerHand.push(tempDeck);
-    renderDeckInContainer(playerHand, dealerContainer);
-  }
-      
-
-//hit functions ->
 function hitPlayerHand() {
-    var tempDeck = shuffledDeck.pop();
-    playerHand.push(tempDeck);
-    renderdeckInContainer(playerHand, handContainer);
-    return playerHand;
+    var hitCard = shuffledDeck.pop();
+    playerHand = playerHand.concat(hitCard);
+    renderDeckInContainer(playerHand, handContainer);
+}
+
+function renderPlayerScore() {
+  
+}
+
+function ArrRenderEx () {
+
 }
 
 /*
-function dealerHand() {
-if i am pressed for time i will generate a random number value between 10 and 22 to compare to playerHand value. 
-}
-*/
-
-
-function renderDeckInContainer(deck, container) {
-  container.innerHTML = '';
-  // Let's build the cards as a string of HTML
-  var cardsHtml = deck.reduce(function(html, card) {
-      console.log('card face',card.face)
-      console.log('card OBJ is', card)
-      // higher order array method or for loop to iterate over hand, this function will include the DOM elements to display the card, and values
-    return html + `<div class="card ${card[0 ].face}"></div>`;
-  }, '');
-  container.innerHTML = cardsHtml;
-}
-
-
-// This will render two cards when game starts
+// This will render two cards when game starts (SAL)
   function renderFirstHandInContainer(deck, container) {
     container.innerHTML = '';
     // Let's build the cards as a string of HTML
@@ -111,20 +148,3 @@ function winLoss() {
   //once player hits or stays, this will compare against this numeric dealer score to determine a win or loss against the dealer 
 }
 */
-
-function buildMasterDeck() {
-  var deck = [];
-  suits.forEach(function(suit) {
-    ranks.forEach(function(rank) {
-      deck.push({
-        // the 'face' property maps to the CSS classes for cards
-        face: `${suit}${rank}`,
-        // the 'value' property is set for blackjack, not war
-        value: Number(rank) || (rank === 'A' ? 11 : 10)
-      });
-    });
-  });
-  return deck;
-}
-
-//renderShuffledDeck();
